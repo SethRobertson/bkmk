324b7196fc87ce0ae4f355cb11ea533c084884b4
James Tanis <jtt@counterstorm.com>
2002-07-30 17:58:55 +0000

    fix bootstrapping problem

--- find_user_var.pl
+++ find_user_var.pl
@@ -4,7 +4,9 @@ $last_inode=0;
 $done=0; 
 while (!$done)
 { 
-  chomp($pwd=`$ARGV[1]`); 
+  # Sigh.. we'd love to autoconf this, but alas we get into a bootstrapping
+  # problem. Well OK, if they don't have pwd, then they're pretty screwed.
+  chomp($pwd=`pwd`); 
   $curdir_inode=(stat($pwd))[1]; 
 
   # infinite recursion protection (not just stepping up to /, but hard links to directories can
