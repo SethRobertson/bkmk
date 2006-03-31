29778822c08e998027a1b34e76cb6b8407afe5f0
Alex Dupuy <dupuy@counterstorm.com>
2006-03-31 00:36:17 +0000

    #6989: make chcopy.pl safe against being run on itself

--- chcopy.pl
+++ chcopy.pl
@@ -1,16 +1,16 @@
 #! /usr/bin/perl -i.bak
-# $Id: chcopy.pl,v 1.14 2006/03/31 00:08:49 dupuy Exp $
+# $Id: chcopy.pl,v 1.15 2006/03/31 00:36:17 dupuy Exp $
 #
-# ++Copyright LIBBK++
+# ++Copyright BAKA++
 #
 # Copyright © 2001-2003,2006 The Authors. All rights reserved.
 #
 # This source code is licensed to you under the terms of the file
 # LICENSE.TXT in this release for further details.
 #
-# Mail <projectbaka@baka.org> for further information
+# Send e-mail to <projectbaka@baka.org> for further information.
 #
-# --Copyright LIBBK--
+# - -Copyright BAKA- -
 #
 #
 # <TODO>This script should be enhanced to maintain existing copyright dates, or
@@ -22,57 +22,55 @@ $YEAR = 1900 + (localtime time)[5];
 
 ######################################################################
 ## BAKA
-$BAKAHDR = <<EOF;
-#if !defined(lint)
-static const char libbk__rcsid[] = "\$Id\$";
-static const char libbk__copyright[] = "Copyright © $YEAR";
-static const char libbk__contact[] = "<projectbaka\@baka.org>";
-#endif /* not lint */
+($BAKAHDR = <<EOF) =~ s/^\|//gm;
+|#if !defined(lint)
+|static const char libbk__rcsid[] = "\$Id\$";
+|static const char libbk__copyright[] = "Copyright © $YEAR";
+|static const char libbk__contact[] = "<projectbaka\@baka.org>";
+|#endif /* not lint */
 EOF
 
-$BAKAPROD = <<EOF;
-++Copyright BAKA++
-
-Copyright © $YEAR The Authors. All rights reserved.
-
-This source code is licensed to you under the terms of the file
-LICENSE.TXT in this release for further details.
-
-Send e-mail to <projectbaka\@baka.org> for further information.
-
-- -Copyright BAKA- -
+($BAKAPROD = "++"."Copyright BAKA++\n".<<EOF) =~ s/^\|//gm;
+|
+|Copyright © $YEAR The Authors. All rights reserved.
+|
+|This source code is licensed to you under the terms of the file
+|LICENSE.TXT in this release for further details.
+|
+|Send e-mail to <projectbaka\@baka.org> for further information.
+|
+|- -Copyright BAKA- -
 EOF
 
 ######################################################################
 ## CounterStorm
-$CSHDR = <<EOF;
-#if !defined(lint)
-static const char cs__rcsid[] = "\$Id\$";
-static const char cs__copyright[] = "Copyright © $YEAR CounterStorm, Inc.";
-static const char cs__contact[] = "CounterStorm <support\@counterstorm.com>";
-#endif /* not lint */
+($CSHDR = <<EOF) =~ s/^\|//gm;
+|#if !defined(lint)
+|static const char cs__rcsid[] = "\$Id\$";
+|static const char cs__copyright[] = "Copyright © $YEAR CounterStorm, Inc.";
+|static const char cs__contact[] = "CounterStorm <support\@counterstorm.com>";
+|#endif /* not lint */
 EOF
 
-$CSPROD = <<EOF;
-++Copyright COUNTERSTORM++
-
-Copyright © $YEAR CounterStorm, Inc.  All rights reserved.
-
-THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF COUNTERSTORM, INC.
-The copyright notice above does not evidence any actual
-or intended publication of such source code.
-
-Only properly authorized employees and contractors of CounterStorm, Inc.
-are authorized to view, possess, or otherwise use this file.
-
-CounterStorm, Inc.
-15 West 26th Street 7th Floor
-New York, NY 10010-1002
-
-+1 212 206 1900
-<support\@counterstorm.com>
-
-- -Copyright COUNTERSTORM- -
+($CSPROD =  "++"."Copyright COUNTERSTORM++\n".<<EOF) =~ s/^\|//gm;
+|
+|Copyright © $YEAR CounterStorm, Inc.  All rights reserved.
+|
+|THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF COUNTERSTORM, INC.
+|The copyright notice above does not evidence any actual
+|or intended publication of such source code.
+|
+|Only properly authorized employees and contractors of CounterStorm, Inc.
+|are authorized to view, possess, or otherwise use this file.
+|
+|CounterStorm, Inc.
+|15 West 26th Street 7th Floor
+|New York, NY 10010-1002
+|
+|+1 212 206 1900
+|<support\@counterstorm.com>
+|
+|- -Copyright COUNTERSTORM- -
 EOF
 
 
